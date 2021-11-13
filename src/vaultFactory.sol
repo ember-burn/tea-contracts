557bea4a9d3234f4778aabbe8f283caa7bcd027d
ember-burn <89837718+ember-burn@users.noreply.github.com>
2021-11-13 14:42:52 +0000

    Update vaultFactory.sol

--- src/vaultFactory.sol
+++ src/vaultFactory.sol
@@ -1,8 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+//OpenZeppelin
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol';
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+
+//homemade
+import './src/vault.sol'
 
 contract VaultFactory is Ownable {
     
