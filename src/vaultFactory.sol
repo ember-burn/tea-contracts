21bdf9ae0d1d0dc03ed4b4bf2b910e7202241ff6
ember-burn <89837718+ember-burn@users.noreply.github.com>
2021-11-13 14:27:53 +0000

    Update vaultFactory.sol

--- src/vaultFactory.sol
+++ src/vaultFactory.sol
@@ -1,4 +1,3 @@
-//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol';
@@ -6,11 +5,29 @@ import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract VaultFactory is Ownable {
     
+    uint256 public vaultCounter;
+    
     event nftTransfer(address to, uint256 tokenID, address tokenContract);
     
+    struct Vault {
+        //address vaultAddress; will be used once child contract function implemented
+        uint256 vaultId;
+        string vaultName;
+        string vaultDescription;
+        address creator;
+    }
+    
+    mapping(uint256 => Vault) public vaultAddresses;
+    
     //test function to experiment with erc721 transfer
-    function transfer(address _to, uint256 _tokenID, address _tokenContract) public {
+    function newVault(address _to, uint256 _tokenID, address _tokenContract, string memory _name, string memory _description) public {
         IERC721(_tokenContract).safeTransferFrom(msg.sender, _to, _tokenID);
+        vaultAddresses[vaultCounter] = Vault(vaultCounter, _name, _description, msg.sender);
         emit nftTransfer(_to, _tokenID, _tokenContract);
+        vaultCounter++;
+    }
+    
+    function returnVaultCount() public view returns(uint256) {
+        return vaultCounter;
     }
 }
