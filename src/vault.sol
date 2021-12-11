632a31eab426e622d0c287871d1c002294c4c35b
ember-burn <89837718+ember-burn@users.noreply.github.com>
2021-12-11 16:33:14 +0000

    Update vault.sol

--- src/vault.sol
+++ src/vault.sol
@@ -1,1 +1,23 @@
 
+pragma solidity ^0.8.10;
+//SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/ownable.sol";
+
+contract Vault is ERC20, Ownable {
+    bool public isCalled = False;
+
+    event BuyoutInitiated();
+    event FractionsCreated();
+
+    function placeBid(uint256 _amount) {
+        
+    }
+
+    function createFractions(uint256 _amount) {
+        require(isCalled == False, "Fractions already claimed")
+        _mint(msg.sender, _amount);
+        isCalled = true;
+    }
+}
